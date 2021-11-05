73fc2a4a9d23d29ac1add6ec6b2648d7a41c5df9
do-Va <varisdogukan@outlook.com>
2021-11-05 13:01:29 +0300

    Added files for homepage

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './homepage.styles.scss';
+
 const HomePage = () => {
   return (
     <div className="homepage">
@@ -19,12 +21,6 @@ const HomePage = () => {
         <div className="menu-item">
           <div className="content">
             <h1 className="title">SNEAKERS</h1>
-            <span className="title">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
             <span className="title">SHOP NOW</span>
           </div>
         </div>
